34673c4dc216ae5e4f68ec03203a690a929edf07
Papaz228 <alansun2002@gmail.com>
2022-07-22 17:01:47 +0600

    DELETE PRODUCT AND ORDER IN ORDERDETAIL

--- src/main/java/com/company/boardgamesshop/entity/OrderDetail.java
+++ src/main/java/com/company/boardgamesshop/entity/OrderDetail.java
@@ -2,14 +2,10 @@ package com.company.boardgamesshop.entity;
 public class OrderDetail {
     private Long id;
     private Long orderId;
-    private Order order;
     private Long productId;
-    private Product product;
     private Integer count;
     private Integer cost;
-    public Long getId() {
-        return id;
-    }
+    public Long getId() {return id;}
     public void setId(Long id) {
         this.id = id;
     }
@@ -19,23 +15,11 @@ public class OrderDetail {
     public void setOrderId(Long orderId) {
         this.orderId = orderId;
     }
-    public Order getOrder() {
-        return order;
-    }
-    public void setOrder(Order order) {
-        this.order = order;
-    }
     public Long getProductId() {
         return productId;
     }
     public void setProductId(Long productId) {
         this.productId = productId;
-    }
-    public Product getProduct() {
-        return product;
-    }
-    public void setProduct(Product product) {
-        this.product = product;
     }
     public Integer getCount() {
         return count;
@@ -54,9 +38,7 @@ public class OrderDetail {
         return "OrderDetail{" +
                 "id=" + id +
                 ", orderId=" + orderId +
-                ", order=" + order +
                 ", productId=" + productId +
-                ", product=" + product +
                 ", count=" + count +
                 ", cost=" + cost +
                 '}';
